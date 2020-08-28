206ab0d2fb58c05d17b58532e7461f4d33f38374
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-08-28 16:10:12 +0000

    Refactored the code except the features

--- index.js
+++ index.js
@@ -1,29 +1,92 @@
 let balance = 500.00;
 
-class Withdrawal {
+/*class Withdrawal {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
+  }
+
+}
+  class Deposit {
+
+    constructor(amount, account) {
+      this.amount  = amount;
+      this.account = account;
+    }
+
+    commit() {
+      this.account.balance += this.amount;
+    }
+
+  }*/
+
+
+  class Account {
+
+    constructor(username) {
+      this.username = username;
+      //have the account balance start at $0 since it makes sense
+      this.balance = 0;
+    }
+  }
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
+  commit() {
+    this.account.balance += this.value;
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount
   }
 
 }
 
+class Withdrawal extends Transaction {
 
+  get value() {
+    return -this.amount
+  }
 
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("Billy Bob");
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
+t1.commit();
+
+const t2 = new Withdrawal(50.00, myAccount);
+t2.commit();
+
+console.log('Ending Balance:', myAccount.balance);
+
+
+
+/*t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
 console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);*/
